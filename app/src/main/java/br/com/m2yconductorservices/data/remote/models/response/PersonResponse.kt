a135467c309f979f96de7eb886c3116f0a4d555f
Golias <joao.victor@mobile2you.com.br>
2019-04-10 19:34:45 -0300

    added nullable properties

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/PersonResponse.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/PersonResponse.kt
@@ -2,14 +2,14 @@ package br.com.m2yconductorservices.data.remote.models.response
 
 class PersonResponse(
     val id: Int,
-    val nome: String,
+    val nome: String?,
     val tipo: String,
-    val cpf: String,
-    val cnpj: String,
-    val dataNascimento: String,
-    val sexo: String,
-    val numeroIdentidade: String,
-    val orgaoExpedidorIdentidade: String,
-    val unidadeFederativaIdentidade: String,
-    val dataEmissaoIdentidade: String
+    val cpf: String?,
+    val cnpj: String?,
+    val dataNascimento: String?,
+    val sexo: String?,
+    val numeroIdentidade: String?,
+    val orgaoExpedidorIdentidade: String?,
+    val unidadeFederativaIdentidade: String?,
+    val dataEmissaoIdentidade: String?
 )
