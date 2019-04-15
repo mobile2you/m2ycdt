587ffdf26c159cd6991a3ba3ca7885ef29e1fb5e
Golias <joao.victor@mobile2you.com.br>
2019-04-15 12:42:42 -0300

    added body to getTicketsRequest

--- app/src/main/java/br/com/m2yconductorservices/data/remote/services/M2YCDTBilletService.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/services/M2YCDTBilletService.kt
@@ -1,6 +1,7 @@
 package br.com.m2yconductorservices.data.remote.services
 
 import br.com.m2yconductorservices.M2YCDTNetworkConstants
+import br.com.m2yconductorservices.data.remote.models.request.GetTicketsRequest
 import br.com.m2yconductorservices.data.remote.models.request.TicketGenerateRequest
 import br.com.m2yconductorservices.data.remote.models.response.TicketGenerateResponse
 import br.com.m2yconductorservices.data.remote.models.response.TicketGeneratedResponse
@@ -14,5 +15,5 @@ interface M2YCDTBilletService {
     fun generateTicket(@Body request: TicketGenerateRequest) : Single<TicketGeneratedResponse>
 
     @POST("${M2YCDTNetworkConstants.CDT_BILLET_URL}getTickets")
-    fun getTickets() : Single<List<TicketGenerateResponse>>
+    fun getTickets(@Body getTicketsRequest: GetTicketsRequest) : Single<List<TicketGenerateResponse>>
 }
