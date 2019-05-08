38ba2730cce51c5b1935640642adf32f8d37c9eb
Kirsten <gustavo@mobile2you.com.br>
2019-05-08 19:14:48 -0300

    Fixed request and version bump

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/request/RateRequest.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/request/RateRequest.kt
@@ -1,9 +1,13 @@
 package br.com.m2yconductorservices.data.remote.models.request
 
-sealed class RateRequest(val serverTypeId: Int) {
+import br.com.m2yconductorservices.data.local.M2YCDTPersistUserInformation
+
+sealed class RateRequest(val operationType: Int, val accountId: String = M2YCDTPersistUserInformation.accountId()) {
     class P2P : RateRequest(1)
     class PAYMENT : RateRequest(2)
     class BANK_TRANSFER : RateRequest(3)
     class TICKET : RateRequest(4)
-    class RECHARGE : RateRequest(9)
+    class PHONE_RECHARGE : RateRequest(5)
+    class SPTRANS_RECHARGE : RateRequest(6)
+    class TICKET_PAID : RateRequest(7)
 }
