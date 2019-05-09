15431bc94cc0ae9854944a6b4251e9396e5f1737
Golias <joao.victor@mobile2you.com.br>
2019-05-09 11:48:07 -0300

    fixed invoices request

--- app/src/main/java/br/com/m2yconductorservices/data/remote/services/M2YCDTInvoicesService.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/services/M2YCDTInvoicesService.kt
@@ -1,11 +1,12 @@
 package br.com.m2yconductorservices.data.remote.services
 
+import br.com.m2yconductorservices.M2YCDTNetworkConstants
 import br.com.m2yconductorservices.data.remote.models.response.GetInvoicesResponse
 import io.reactivex.Single
 import retrofit2.http.POST
 
 interface M2YCDTInvoicesService {
 
-    @POST("getInvoices")
+    @POST("${M2YCDTNetworkConstants.CDT_INVOICES}getInvoices")
     fun getInvoices(): Single<GetInvoicesResponse>
 }
