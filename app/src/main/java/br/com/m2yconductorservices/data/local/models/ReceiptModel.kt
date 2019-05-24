97d34a1ed34a2f6ee8b03f73ff6e298ec3fdc1fd
Kirsten <gustavo@mobile2you.com.br>
2019-05-24 18:18:40 -0300

    Fixed bilhete model again

--- app/src/main/java/br/com/m2yconductorservices/data/local/models/ReceiptModel.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/local/models/ReceiptModel.kt
@@ -16,7 +16,7 @@ data class ReceiptModel(
     val transfer: ReceiptTransferModel? = null,
     val payment: ReceiptPaymentModel? = null,
     val recharge: ReceiptRechargeModel? = null,
-    val bilhete: ReceiptRechargeModel? = null,
+    val bilhete: ReceiptBilheteModel? = null,
     val description: String? = null
 ) : Serializable
 
