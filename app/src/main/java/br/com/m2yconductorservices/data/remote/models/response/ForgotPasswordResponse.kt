b226582659bfb77347d2a4f6b4dffe553e701a9d
Kirsten <gustavo@mobile2you.com.br>
2019-05-27 16:45:56 -0300

    Added email to forgotpasswordresponse

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/ForgotPasswordResponse.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/ForgotPasswordResponse.kt
@@ -1,3 +1,6 @@
 package br.com.m2yconductorservices.data.remote.models.response
 
-data class ForgotPasswordResponse(val phone: String)
+data class ForgotPasswordResponse(
+    val phone: String,
+    val email: String?
+)
