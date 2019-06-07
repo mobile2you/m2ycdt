15395077989bee7468bc5959ad3103e820a7952b
Leonardo Luque <leonardo.luque@mobile2you.com.br>
2019-06-07 17:51:05 -0300

    add serializable in bank model

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/BankModel.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/BankModel.kt
@@ -1,8 +1,10 @@
 package br.com.m2yconductorservices.data.remote.models.response
 
+import java.io.Serializable
+
 data class BankModel(
         val id: String,
         val name: String,
         val color: String,
         val code: Int
-)
+) : Serializable
