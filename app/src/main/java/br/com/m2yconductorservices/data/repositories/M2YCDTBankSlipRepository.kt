d6b2860227ca6516e3e08930f562516b3e0dde50
Leonardo Luque <leonardo.luque@mobile2you.com.br>
2019-07-31 11:06:42 -0300

    fix getPdf and bump version

--- app/src/main/java/br/com/m2yconductorservices/data/repositories/M2YCDTBankSlipRepository.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/repositories/M2YCDTBankSlipRepository.kt
@@ -7,9 +7,9 @@ import java.io.File
 
 object M2YCDTBankSlipRepository {
 
-    fun getPdf(ticketId: IdIntRequest?, storageDir: File) = getPdf(ticketId, storageDir, ticketId?.id.toString())
+    fun getPdf(ticketId: IdIntRequest?, storageDir: File) = getPdfExtension(ticketId, storageDir, ticketId?.id.toString())
 
-
+    @Deprecated("Must no longer be used, use pdf with extension", replaceWith = ReplaceWith("getPdfExtension"))
     fun getPdf(ticketId: IdIntRequest?, storageDir: File, fileName: String) = M2YCDTBankSlipRemoteDataSource.getPDF(ticketId)
     .map {
         val file = File(storageDir, fileName)
@@ -19,4 +19,19 @@ object M2YCDTBankSlipRepository {
         sink.close()
         file
     }
+
+    fun getPdfExtension(ticketId: IdIntRequest?, storageDir: File, fileName: String) = M2YCDTBankSlipRemoteDataSource.getPDF(ticketId)
+        .map {
+            val file = File.createTempFile(
+                "${ticketId?.id}", /* prefix */
+                ".pdf", /* suffix */
+                storageDir      /* directory */
+            )
+            val sink = Okio.buffer(Okio.sink(file))
+            // you can access body of response
+            sink.writeAll(it.body().source())
+            sink.close()
+            file
+        }
+
 }
