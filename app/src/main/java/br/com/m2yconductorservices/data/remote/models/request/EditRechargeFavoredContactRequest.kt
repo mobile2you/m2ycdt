dd6a3af38738bdb9ccca8b9f3036b031fc463f53
Fernando Batista <fernandohsbatista@gmail.com>
2019-10-02 11:47:55 -0300

    Fixed request model for editing a favorite contact of recharges.

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/request/EditRechargeFavoredContactRequest.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/request/EditRechargeFavoredContactRequest.kt
@@ -1,9 +1,6 @@
 package br.com.m2yconductorservices.data.remote.models.request
 
 data class EditRechargeFavoredContactRequest (
-    val favorite_id: String,
-    val ddd: String,
-    val phone: String,
-    val mobile_carrier: String,
+    val id: String,
     val nickname: String
 )
