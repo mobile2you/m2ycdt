ae0d3321db741bdcd5d7b07b4062bda824bed33c
Leo Almeida <leonam.almeida@mobile2you.com.br>
2019-10-03 11:48:13 -0300

    Zip endereco and bairro fields now can be null

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/CepResponse.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/CepResponse.kt
@@ -2,10 +2,10 @@ package br.com.m2yconductorservices.data.remote.models.response
 
 import java.io.Serializable
 
-data class CepResponse(val cep: String,
-                       val endereco: String,
-                       val bairro: String,
-                       val cidade: String,
-                       val uf: String
-
-): Serializable
+data class CepResponse(
+    val cep: String,
+    val endereco: String?,
+    val bairro: String?,
+    val cidade: String,
+    val uf: String
+) : Serializable
