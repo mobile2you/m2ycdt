e6b5e77252ffcde495be776babb4906b6c4bcca9
Paiva <lucas.paiva@mobile2you.com.br>
2019-11-19 11:42:05 -0300

    Add Serializable to P2PResponse

--- app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/P2PResponse.kt
+++ app/src/main/java/br/com/m2yconductorservices/data/remote/models/response/P2PResponse.kt
@@ -1,9 +1,11 @@
 package br.com.m2yconductorservices.data.remote.models.response
 
+import java.io.Serializable
+
 data class P2PResponse (
         var name: String?,
         var cpf: String?,
         var account: String?,
         var nickname: String?,
         var color: String?
-)
+) : Serializable
